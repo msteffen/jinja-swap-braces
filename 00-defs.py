cfd46a781978cf02c4de783be72fb2483d024832
msteffen <matthew.steffen@gmail.com>
2015-09-01 21:15:50 -0700

    Update comments with an example

--- 00-defs.py
+++ 00-defs.py
@@ -4,6 +4,23 @@ import template_utils
 def gencmd(cmd):
   return jinja2.Template(template_utils.swap_braces(cmd)).render(**globals())
 
+# Example:
+# in your ipython notebook:
+# -----
+# user = 'msteffen'
+# instance = 'dev'
+# -----
+# printcmds('run_binary'
+#   ' {% if instance == 'dev' %}192.168.0.1{% else %}10.0.13.2{% endif %}'
+#   ' --user={user}'
+#   ' --binary=${{BINARY}}',
+#   BINARY='~/bin/{instance}_binary')
+# -----
+#
+# This prints out:
+#   BINARY="~/bin/dev_binary"
+#   run_binary 192.168.0.1 --user=msteffen --binary=${BINARY}
+# As the cell result in ipython, for easy copying and pasting (or further tweaking)
 def printcmds(*cmds, **vardefs):
   spcr = template_utils.Spacer();
   for k, v in vardefs.iteritems():
