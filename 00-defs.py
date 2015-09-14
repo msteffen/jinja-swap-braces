2ea6efbca541b9ce9882c8c3dc30f3ff8961d920
msteffen <matthew.steffen@gmail.com>
2015-09-13 21:52:21 -0700

    Remove extraneous spaces and such

--- 00-defs.py
+++ 00-defs.py
@@ -1,56 +1,53 @@
-import jinja2                                                                       
-import itertools                                                                    
+import jinja2
+import itertools
 
-import sys                                                                          
+import sys
 sys.path.append('...path/to/template_utils.py')
-import template_utils                                                               
+import template_utils
 
-jcg = "java/com/google"                                                             
-jt  = "javatests/com/google"                                                        
+def nz(s):
+  return s is not None and len(s) > 0
 
-def nz(s):                                                                          
-  return s is not None and len(s) > 0                                               
+def allnz(*ss):
+  return all([ nz(s) for s in ss ])
 
-def allnz(*ss):                                                                     
-  return all([ nz(s) for s in ss ])                                                 
+def anynz(*ss):
+  return all([ nz(s) for s in ss ])
 
-def anynz(*ss):                                                                     
-  return all([ nz(s) for s in ss ])                                                 
+def format_list(l, s):
+  return s.format(*l)
 
-def format_list(l, s):                                                              
-  return s.format(*l)                                                               
+def format_dict(d, s):
+  return s.format(**d)
 
-def format_dict(d, s):                                                              
-  return s.format(**d)                                                              
+# class QuantifierExtension(Extension):
+#   tags = set(['&&', '||', '&&/' '||/'])
+#                                           
+#   def parse(self, parser):
+#     tok = next(parser.stream)
+#
+#     if (tok.value is '&&/'):
+#       ast = parse_statements(['name:/&&'], drop_needle=True)
+#       # TODO look at variables and if all variables are defined, print it
+#     elif tok.value is '||/':
+#       ast = parse_statements(['name:/||'], drop_needle=True)
+#       # TODO look at variables and if any variables are defined, print it
 
-# class QnatifierExtension(Extension):                                              
-#   tags = set(['&&', '||', '&&/' '||/'])                                           
-#                                                                                   
-#   def parse(self, parser):                                                        
-#     tok = next(parser.stream)                                                     
-#                                                                                   
-#     if (tok.value is '&&/'):                                                      
-#       ast = parse_statements(['name:/&&'], drop_needle=True)                      
-#       # TODO look at variables and if all variables are defined, print it         
-#     elif tok.value is '||/':                                                      
-#       ast = parse_statements(['name:/||'], drop_needle=True)                      
-#       # TODO look at variables and if any variables are defined, print it         
+env = jinja2.Environment(lstrip_blocks=True, trim_blocks=True,
+                         extensions=['jinja2.ext.loopcontrols'])
+env.filters.update({
+  'nonzero': nz,
+  'allnz'  : allnz,
+  'anynz'  : anynz,
+  'fmtl'   : format_list,
+  'fmtd'   : format_dict,
+})
 
-env = jinja2.Environment(lstrip_blocks=True, trim_blocks=True,                      
-                         extensions=['jinja2.ext.loopcontrols'])                    
-env.filters.update({                                                                
-  'nonzero': nz,                                                                    
-  'allnz'  : allnz,                                                                 
-  'anynz'  : anynz,                                                                 
-  'fmtl'   : format_list,                                                           
-  'fmtd'   : format_dict,                                                           
-})                                                                                  
+def printglobal(key):
+  print str(globals()[key])
 
-def printglobal(key):                                                               
-  print str(globals()[key])                                                         
-
-def gencmd(cmd):                                                                    
-  return env.from_string(template_utils.swap_braces(cmd)).render(**globals())       
+def gencmd(cmd):
+  return env.from_string(template_utils.swap_braces(cmd)).render(**globals())
 
 # Example:
 # in your ipython notebook:
@@ -69,8 +66,9 @@ def gencmd(cmd):
 #   BINARY="~/bin/dev_binary"
 #   run_binary 192.168.0.1 --user=msteffen --binary=${BINARY}
 # As the cell result in ipython, for easy copying and pasting (or further tweaking)
-def printcmds(*cmds, **vardefs):                                                    
-  print '\n\n'.join(itertools.chain(                                                
-    [ '{}={}'.format(k, gencmd(v)) for k, v in vardefs.iteritems() ],               
-    [ gencmd(c) for c in cmds ],                                                    
-  ))                                     
+def printcmds(*cmds, **vardefs):
+  print '\n\n'.join(itertools.chain(
+    [ '{}={}'.format(k, gencmd(v)) for k, v in vardefs.iteritems() ],
+    [ gencmd(c) for c in cmds ],
+  ))
+  
